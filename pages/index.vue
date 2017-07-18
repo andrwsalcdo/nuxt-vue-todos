e59d3003013f0a2ff685abf5ae0bf12f148f909e
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-17 20:29:51 -0400

    add Store. display state of counter

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    hello world
+    {{ counter }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      counter: state => state.counter
+    })
+  }
+}
 </script>
