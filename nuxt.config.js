2f29fea26b5396448f64f624b92d17c33a41736d
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-17 20:53:30 -0400

    add main css & tachyon css lib via npm install

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,10 @@ module.exports = {
     ]
   },
   /*
+  ** Global CSS
+  */
+  css: ['~assets/css/main.css', 'tachyons/css/tachyons.css'],
+  /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
