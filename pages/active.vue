0f7439bff62baec87b5fa3005d21433164419fae
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-31 11:12:23 -0400

    add active & completed pages

--- pages/active.vue
+++ pages/active.vue
@@ -1,3 +1,28 @@
 <template>
-    <div></div>
+  <div>
+    <div class="pa3 pa5-ns"> 
+        <h1 class="f4 bold center mw6">Active Todos</h1>      
+        <ul class="list pl0 measure center">
+          <li v-for="todo of todos" class="flex items-center lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" >
+            <span v-bind:class="{strike: todo.complete}" class="flex-auto"> {{todo.id}} {{ todo.task }} </span>
+          </li>
+        </ul>
+      </div>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import { init } from './sharedData'
+
+export default {
+  fetch: init,
+
+  computed: {
+    ...mapState({
+      todos: state => state.todos.filter(todo => !todo.complete)
+    })
+  }
+}
+</script>
+
--- pages/completed.vue
+++ pages/completed.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="pa3 pa5-ns"> 
-        <h1 class="f4 bold center mw6">Todos</h1>      
+        <h1 class="f4 bold center mw6">Completed Todos</h1>      
         <ul class="list pl0 measure center">
-            <li v-for="todo of todos" class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" >
-            {{ todo.task }}  
-            </li>
+          <li v-for="todo of todos" class="flex items-center lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" >
+            <span class="flex-auto"> {{todo.id}} {{ todo.task }} </span>
+          </li>
         </ul>
-    </div>
+      </div>
   </div>
 </template>
 
@@ -20,12 +20,8 @@ export default {
 
   computed: {
     ...mapState({
-      todos: state => state.todos
+      todos: state => state.todos.filter(todo => todo.complete)
     })
-  },
-
-  methods: {
-
   }
 }
 </script>
